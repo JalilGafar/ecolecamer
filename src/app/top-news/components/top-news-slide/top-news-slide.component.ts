31174b76419ab631f083e03cd06fbe0d756bd285
Fonda Njiemessa Jalil Gafar <fondajalil@hotmail.com>
2022-12-26 08:30:42 +0100

    Mise en page banner-stat

--- src/app/top-news/components/top-news-slide/top-news-slide.component.ts
+++ src/app/top-news/components/top-news-slide/top-news-slide.component.ts
@@ -10,6 +10,27 @@ import { SharedModule } from 'src/app/shared/shared.module';
 export class TopNewsSlideComponent {
 
   myTopNews!: TopNews[];
+  responsiveOptions;
+
+  constructor(){
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 3,
+          numScroll: 3
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 2,
+          numScroll: 2
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+          numScroll: 1
+      }
+    ];
+  }
 
   ngOnInit() {
     this.myTopNews = [
